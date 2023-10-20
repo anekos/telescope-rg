7801bcc2006f7a7706b7a744a9fb86a271a83aec
anekos <anekos@snca.net>
2023-10-20 16:30:30 +0900

    Export the picker

--- lua/telescope/_extensions/rg.lua
+++ lua/telescope/_extensions/rg.lua
@@ -1,14 +1,11 @@
-local telescope_installed, telescope = pcall(require, 'telescope')
+local telescope = require('telescope')
 
-if not telescope_installed then
-    error('This plugin requires nvim-telescope/telescope.nvim')
-end
-
-local picker = require('telescope._extensions.rg.picker')
+local picker = require('telescope-rg.picker')
 
 return telescope.register_extension({
   setup = function(ext_conf, conf)
   end,
   exports = {
+    rg = picker,
   },
 })
